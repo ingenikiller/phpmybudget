d2588b6fbdfc1150bdf07a62863d25fb19a99e06
ingeni <ingeni_killer@hotmail.com>
2026-08-16 21:03:01 +0200

    modif api.php

--- api.php
+++ api.php
@@ -18,7 +18,7 @@ define('CHEMIN_LOGERREUR', './logs/');
 define('LIGNE_PAR_PAGE', 40);
 define('RACINE_DATA', 'data/projets');
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: Authorization, Content-Type, X-Requested-With");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Authorization, Content-Type, X-Requested-With");
 $pageControl = new PageControl(TRUE);
 $pageControl->process();
